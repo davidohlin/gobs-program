77a3e9d1172105438ccb36258e8126102a2cb0bf
David Öhlin <hello@davidohlin.se>
2016-07-29 15:41:06 +0200

    Initial commit

---
+++ cli.js
@@ -0,0 +1,25 @@
+'use strict'
+const yesno = require('yesno');
+
+let program = () => {
+	for ( let i=1; i <= 204; i++ ) {
+		setTimeout (() => {
+			if (i === 204) {
+				process.stdout.write("Penus ");
+				process.exit();
+			} else if (i % 6 === 0) {
+				process.stdout.write("Penus\n");
+			} else {
+				process.stdout.write("Penus ");
+			}
+		}, i*50 );
+	}
+};
+
+yesno.ask("Gob's Program: Y/N?", true, function(yes) {
+	if(yes) {
+		program();
+	} else {
+		process.exit();
+	}
+});
